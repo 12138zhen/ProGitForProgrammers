b69b52217e4abb62ed64427dd564cb43205e00e4
Zhenyu <1109115258@qq.com>
2022-04-26 15:44:18 +0800

    first commit with command line

---
+++ src/mian.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: mian.cpp
+	> Author: Zhenyu Hong
+	> Mail: hongzh1@udmercy.edu
+	> Created Time: Tue 26 Apr 2022 03:39:34 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int mian() {
+  cout << "hi" << endl;
+  return 0;
+}
